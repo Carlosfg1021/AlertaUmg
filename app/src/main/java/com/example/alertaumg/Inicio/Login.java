3c402808929b488058d7e5bdb4a310430071eb95
Carlos Daniel Franco Pérez <carlosfg1021@gmail.com>
2020-09-28 11:44:41 -0600

    Initial commit

---
+++ app/src/main/java/com/example/alertaumg/Inicio/Login.java
@@ -0,0 +1,16 @@
+package com.example.alertaumg.Inicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.alertaumg.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/alertaumg/Inicio/Portada.java
@@ -0,0 +1,27 @@
+package com.example.alertaumg.Inicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.alertaumg.R;
+
+public class Portada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portada);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Portada.this, Login.class);
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+}
