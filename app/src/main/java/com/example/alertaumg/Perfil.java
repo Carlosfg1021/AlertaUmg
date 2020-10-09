04ae2ec8787013f5c8fd0776f8f9f06992c77ff1
AJMG3011 <62410887+ajmg3011@users.noreply.github.com>
2020-10-08 18:32:18 -0600

    Activity Editar Perfil

--- app/src/main/java/com/example/alertaumg/Perfil.java
+++ app/src/main/java/com/example/alertaumg/Perfil.java
@@ -1,12 +1,16 @@
 package com.example.alertaumg;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 
 /**
  * A simple {@link Fragment} subclass.
