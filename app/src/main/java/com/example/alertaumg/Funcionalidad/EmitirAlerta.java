ee67d2c36a6035ed25aedc67f0db6c3a75c4234b
Carlos Daniel Franco Pérez <carlosfg1021@gmail.com>
2020-10-19 21:22:10 -0600

    Mejorado

--- app/src/main/java/com/example/alertaumg/Funcionalidad/EmitirAlerta.java
+++ app/src/main/java/com/example/alertaumg/Funcionalidad/EmitirAlerta.java
@@ -2,15 +2,56 @@ package com.example.alertaumg.Funcionalidad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.alertaumg.R;
 
 public class EmitirAlerta extends AppCompatActivity {
 
+    ImageView imageView;
+    Button botonfoto;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emitir_alerta);
+
+        botonfoto = (Button) findViewById(R.id.botonfoto);
+        imageView = (ImageView) findViewById(R.id.imagenalerta);
+
+        botonfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                llamarIn();
+            }
+        });
+
+    }
+
+    public void llamarIn(){
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imagenBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imagenBitmap);
+        }
+
     }
 }
--- app/src/main/java/com/example/alertaumg/Inicio/Portada.java
+++ app/src/main/java/com/example/alertaumg/Inicio/Portada.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.alertaumg.Funcionalidad.EmitirAlerta;
 import com.example.alertaumg.R;
 
 public class Portada extends AppCompatActivity {
